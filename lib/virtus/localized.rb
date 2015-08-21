b836fc4a5a0920fa808cce5a22dde13cbb70dd67
xescuGC <xescugil@gmail.com>
2015-08-21 23:29:41 +0200

    Changed the way it included the dependecies

--- lib/virtus/localized.rb
+++ lib/virtus/localized.rb
@@ -1,7 +1,7 @@
 require "virtus/localized/version"
 require 'virtus'
-require 'virtus/localized/attribute_set'
-require 'virtus/localized/attribute'
+require_relative 'localized/attribute_set'
+require_relative 'localized/attribute'
 require 'i18n'
 
 I18n.enforce_available_locales = false
